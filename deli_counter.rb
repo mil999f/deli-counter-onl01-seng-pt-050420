fd3f99e9e181ed37b1c9125015af096c1514e35e
Milly Flores <miflo0h@gmail.com>
2020-05-16 01:59:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ def now_serving(array)
     "if deli.empty"
     puts "There is nobody waiting to be served!" 
   else 
-    puts "currently serving #{array[0]}"
+    puts "Currently serving #{array[0]}"
   end
 end
