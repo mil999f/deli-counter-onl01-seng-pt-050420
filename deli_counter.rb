fa14e417e86fdd925ea8f6058b42aa05adbebc01
Milly Flores <miflo0h@gmail.com>
2020-05-16 01:48:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ end
 def now_serving(array)
   if array.length == 0
     "if deli.empty"
-  puts "There is nobody waiting to be served!" 
-  
+    puts "There is nobody waiting to be served!" 
+  end
 end
