55fdfdebce9b0e2b252715f3caf98f71c04eac07
Milly Flores <miflo0h@gmail.com>
2020-05-16 01:38:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(cust)
   serving = []
   if cust.size == 0 
     puts "The ling is currently empty."
-  elsif customers.size >= 1 
+  elsif cust.size >= 1 
   cust.each_with_index do |cust, index|
     serving << "#{index +1}. #{cust}"
     puts "The line is currently: #{serving.join(' ')}"
@@ -13,5 +13,5 @@ end
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
-  puts "Welcome, #{name}.  You are number #{array.index(name)+1} in line."
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
 end
